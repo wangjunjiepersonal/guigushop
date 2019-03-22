e2b8c28d59b965e508959d3139b0518b56310c3c
wangjunjie <310294582@qq.com>
2019-03-22 10:48:45 +0800

    硅谷商城项目

---
+++ src/store/mutations-types.js
@@ -0,0 +1,9 @@
+//
+/*
+ * 包含n个mutations的type名称常量 主要用来actions与 mutations进行交互
+ * 接收地址  接收食品分类  接收商品列表
+ * */
+
+export const RECEIVE_ADDRESS = 'receive_address'
+export const RECEIVE_CATEGORYS = 'receive_categorys'
+export const RECEIVE_SHOPS = 'receive_shops'
